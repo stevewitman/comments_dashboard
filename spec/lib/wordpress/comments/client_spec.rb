a4125dcd6f2bd25533557498c843c61aa30fe546
Steve Witman <stevewitman@gmail.com>
2015-12-06 00:06:40 -0700

    set up first passing example

--- spec/lib/wordpress/comments/client_spec.rb
+++ spec/lib/wordpress/comments/client_spec.rb
@@ -1,10 +1,19 @@
 module Wordpress
 	module Comments
 		class Client
+			attr_reader :url
+
+			def initialize url
+
+				@url = url
+			end
 		end
 	end
 end
 
 describe Wordpress::Comments::Client do
-
+	it "stores a URL" do
+		client = Wordpress::Comments::Client.new 'http://mashable.com/commets/feed'
+		expect(client.url).to eq 'http://mashable.com/commets/feed'
+	end
 end
